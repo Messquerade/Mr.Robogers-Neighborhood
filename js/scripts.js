b12d3bf10c52d020edfe8e14ebe039106fd5f028
Messquerade <annac.klingberg@gmail.com>
2021-08-28 08:51:31 -0700

    WIP: playfulRange().

--- js/scripts.js
+++ js/scripts.js
@@ -4,29 +4,19 @@ function playfulRange(number) {
   if (number === 0) {
     return "0";
   };
-  const stringNumber = number.toString();
-  let playfulArray = []
-  for (const character of stringNumber) {
-    if (character === "3") {
-      return "Won't you be my neighbor?";
-    };
-  };
-  for (const character of stringNumber) {
-    if (character === "2") {
-      return "Boop!";
-    };
-  };
-  for (const character of stringNumber) {
-    if (character === "1") {
-      return "Beep!";
-    };
-  };
-  for (let i = 0; i <= number; i ++) {
-    playfulArray.push(i);
-  };
-  playfulArray[1] = "Beep!";
-  playfulArray[2] = "Boop!";
-  playfulArray[3] = "Won't you be my neighbor?";
+  playfulArray = []
+  for (let i = 0; i <= number; i++) {
+    let playfulNumber = i.toString();
+    console.log(playfulNumber)
+    if (playfulNumber.includes("1")) {
+      playfulNumber = "Beep!"
+    } else if (playfulNumber.includes("2")) {
+      playfulNumber = "Boop!";
+    } else if (playfulNumber.includes("3")) {
+      playfulNumber = "Won't you be my neighbor?";
+    }
+    playfulArray.push(playfulNumber);
+  }
   return playfulArray.join(", ");
 };
 
